77f7abd4b319e23559c49c707b431553e1549056
Agostina Bidegain <agostinabidegain@gmail.com>
2025-07-01 13:53:58 -0300

    add background images and elements at programa section

--- src/app/programa/page.tsx
+++ src/app/programa/page.tsx
@@ -6,6 +6,7 @@ export default function Programa(){
             <SectionTitle
                 section={'Programa'}
                 imgSrc={'/icons/programa.png'}
+                className={'programaTitle'}
             />
         </main>
     )
