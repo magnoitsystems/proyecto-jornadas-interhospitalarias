d130e66858404be4939f97b72fdae47d78dcffdc
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-07-01 14:20:30 -0300

    finalizo nav para desktop con imagen de fondo

--- src/components/Nav-Bar-Component/NavBar.tsx
+++ src/components/Nav-Bar-Component/NavBar.tsx
@@ -4,87 +4,91 @@ import Link from 'next/link';
 import Image from 'next/image';
 import styles from './NavBar.module.css';
 import { JSX } from 'react';
-import {cactus} from "@/app/ui/fonts"
+import { cactus } from "@/app/ui/fonts"
 import { usePathname } from 'next/navigation';
+import { maxHeaderSize } from 'http';
 
 export default function NavBar(): JSX.Element {
 
     const pathname = usePathname();
 
     return (
-        <nav className={styles.navbar}>
-            {/* Logo a la izquierda */}
-            <div className={styles.logo}>
-                <Link href="/">
-                    <Image
-                        src={'/imgs/nav-logo.png'}
-                        alt={'interhospitalities meeting logo'}
-                        width={392}
-                        height={61}
-                        style={{ cursor: 'pointer' }}
-                    />
-                </Link>
-            </div>
+        <section className={styles.heroSection}>
+            <nav className={styles.navbar}>
 
-            {/* Menú a la derecha */}
-            <ul className={styles.menu}>
-        <li>
-          <Link
-            href="/"
-            className={`${styles.homeLink} ${cactus.className} ${pathname === '/' ? styles.activeLink : ''}`}
-          >
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/autoridades"
-            className={`${styles.autoridadesLink} ${cactus.className} ${pathname === '/autoridades' ? styles.activeLink : ''}`}
-          >
-            Autoridades
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/invitados"
-            className={`${styles.invitadosLink} ${cactus.className} ${pathname === '/invitados' ? styles.activeLink : ''}`}
-          >
-            Invitados
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/programa"
-            className={`${styles.programaLink} ${cactus.className} ${pathname === '/programa' ? styles.activeLink : ''}`}
-          >
-            Programa
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/inscripcion"
-            className={`${styles.inscripcionLink} ${cactus.className} ${pathname === '/inscripcion' ? styles.activeLink : ''}`}
-          >
-            Inscripción
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/trabajos"
-            className={`${styles.trabajosLink} ${cactus.className} ${pathname === '/trabajos' ? styles.activeLink : ''}`}
-          >
-            Trabajos
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/sponsors"
-            className={`${styles.sponsorsLink} ${cactus.className} ${pathname === '/sponsors' ? styles.activeLink : ''}`}
-          >
-            Sponsors
-          </Link>
-        </li>
-      </ul>
-        </nav>
+                <div className={styles.logo}>
+                    <Link href="/">
+                        <Image
+                            src={'/imgs/nav-logo.png'}
+                            alt={'interhospitalities meeting logo'}
+                            width={392}
+                            height={61}
+                            style={{ cursor: 'pointer' }}
+                        />
+                    </Link>
+                </div>
+
+                <ul className={styles.menu}>
+                    <li>
+                        <Link
+                            href="/"
+                            className={`${styles.homeLink} ${cactus.className} ${pathname === '/' ? styles.activeLink : ''}`}
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href="/autoridades"
+                            className={`${styles.autoridadesLink} ${cactus.className} ${pathname === '/autoridades' ? styles.activeLink : ''}`}
+                        >
+                            Autoridades
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href="/invitados"
+                            className={`${styles.invitadosLink} ${cactus.className} ${pathname === '/invitados' ? styles.activeLink : ''}`}
+                        >
+                            Invitados
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href="/programa"
+                            className={`${styles.programaLink} ${cactus.className} ${pathname === '/programa' ? styles.activeLink : ''}`}
+                        >
+                            Programa
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href="/inscripcion"
+                            className={`${styles.inscripcionLink} ${cactus.className} ${pathname === '/inscripcion' ? styles.activeLink : ''}`}
+                        >
+                            Inscripción
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href="/trabajos"
+                            className={`${styles.trabajosLink} ${cactus.className} ${pathname === '/trabajos' ? styles.activeLink : ''}`}
+                        >
+                            Trabajos
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href="/sponsors"
+                            className={`${styles.sponsorsLink} ${cactus.className} ${pathname === '/sponsors' ? styles.activeLink : ''}`}
+                        >
+                            Sponsors
+                        </Link>
+                    </li>
+                </ul>
+
+            </nav>
+        </section>
+
     );
 }
