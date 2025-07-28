11901292f1b5d491892ae45e8c081d18be50fb0e
GianFrancoLaplace <laplacegian@gmail.com>
2025-07-28 12:24:40 -0300

    eliminación de código innecesario

--- src/utils/MedicalStatsProcessor.ts
+++ src/utils/MedicalStatsProcessor.ts
@@ -1,5 +1,4 @@
-import { User } from '@/types/user';
-import { StatsByGender, StatsBySpecialty, ProcessedStats, CSVConfig } from '@/types/csv';
+import { ProcessedStats, CSVConfig } from '@/types/csv';
 import { prisma } from '@/libs/prisma';
 
 
@@ -148,72 +147,4 @@ export class MedicalStatsProcessor {
 
 		return result;
 	}
-
-    private processStatsByGender(users: User[]): StatsByGender[] {
-        const genderMap = new Map<string, {
-            users: User[],
-            professionCount: Record<string, number>
-        }>();
-
-        users.forEach(user => {
-            const gender = user.gender || 'no especificado';
-
-            if (!genderMap.has(gender)) {
-                genderMap.set(gender, {
-                    users: [],
-                    professionCount: {}
-                });
-            }
-
-            const genderData = genderMap.get(gender)!;
-            genderData.users.push(user);
-
-            const profession = user.job || 'sin definir';
-            genderData.professionCount[profession] = (genderData.professionCount[profession] || 0) + 1;
-        });
-
-        return Array.from(genderMap.entries()).map(([gender, data]) => ({
-            gender,
-            count: data.users.length,
-            professionBreakdown: data.professionCount
-        }));
-    }
-
-    private processStatsBySpecialty(users: User[]): StatsBySpecialty[] {
-        const usersWithSpecialty = users.filter(user => user.specialty);
-
-        const specialtyMap = new Map<string, {
-            users: User[],
-            professionCount: Record<string, number>,
-            genderCount: Record<string, number>
-        }>();
-
-        usersWithSpecialty.forEach(user => {
-            const specialty = user.specialty!;
-
-            if (!specialtyMap.has(specialty)) {
-                specialtyMap.set(specialty, {
-                    users: [],
-                    professionCount: {},
-                    genderCount: {}
-                });
-            }
-
-            const specialtyData = specialtyMap.get(specialty)!;
-            specialtyData.users.push(user);
-
-            const profession = user.job || 'sin definir';
-            specialtyData.professionCount[profession] = (specialtyData.professionCount[profession] || 0) + 1;
-
-            const gender = user.gender || 'no especificado';
-            specialtyData.genderCount[gender] = (specialtyData.genderCount[gender] || 0) + 1;
-        });
-
-        return Array.from(specialtyMap.entries()).map(([specialty, data]) => ({
-            specialty,
-            count: data.users.length,
-            professions: data.professionCount,
-            genderBreakdown: data.genderCount
-        }));
-    }
 }
