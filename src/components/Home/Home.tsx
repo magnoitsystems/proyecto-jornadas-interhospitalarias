d30e485144fb46ab456a43795e198c179f41db33
Agostina Bidegain <agostinabidegain@gmail.com>
2025-07-31 15:00:59 -0300

    added future sponsors

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import InfoCard from "@/components/Home/InfoCards/InfoCard";
 import CardAndSponsors from "@/components/Home/CardAndSponsors/CardAndSponsors";
 import { useState } from 'react';
+import { nunito } from '@/app/(views)/ui/fonts';
 import { cactus } from '@/app/(views)/ui/fonts';
 
 export default function Home(){
@@ -33,8 +34,8 @@ export default function Home(){
     ];
 
     return(
-        <div>
-            <section className={`${styles.sectionCardsProperties}`}>
+        <main className={styles.main}>
+            <section className={`${styles.sectionCardsProperties} ${nunito.className}`}>
                 <div className={`${styles.cardCompletyProperties} ${abierta? styles.openCardCardCompletyProperties : styles.cardCompletyProperties}`}  onMouseEnter={() => setHovered(true)}
                      onMouseLeave={() => setHovered(false)}>
 
@@ -45,7 +46,7 @@ export default function Home(){
                             </button>
                         </div>
 
-                        <div className={`${styles.todo} ${cactus.className}`}>
+                        <div className={`${styles.todo} ${nunito.className}`}>
                             <div className={styles.imageCardProperties}>
                                 <Image src={'/icons/homeIcon.png'} alt={"Icono de casa"} width={32} height={32}/>
                             </div>
@@ -56,7 +57,7 @@ export default function Home(){
                         </div>
                     </div>
 
-                    <div className={`${styles.infoCardProperties} ${abierta? styles.openCard : styles.cardClosed}`}>
+                    <div className={`${styles.infoCardProperties} ${nunito.className} ${abierta? styles.openCard : styles.cardClosed}`}>
                         <InfoCard info="Teatro del Fuerte" info2={"MUMBAT"} info3={"Salón Blanco del Palacio Municipal"}
                                   info4={"Htal. de Niños Debilio Blanco Villegas"} sede={true} url={url}/>
                     </div>
@@ -69,7 +70,7 @@ export default function Home(){
                                 <Image src={'/icons/deployBlackIcon.png'} alt={"Icono para desplegar la card con información"} width={25} height={15} />
                             </button>
                         </div>
-                        <div className={`${styles.todo} ${cactus.className}`}>
+                        <div className={`${styles.todo} ${nunito.className}`}>
                             <div className={`${styles.imageCardProperties} ${styles.imageCalendarCardProperties}`}>
                                 <Image src={'/icons/calendarIcon.png'} alt={"Icono de calendario"} width={32} height={32}/>
                             </div>
@@ -79,7 +80,7 @@ export default function Home(){
                             </div>
                         </div>
                     </div>
-                    <div className={`${styles.infoCardProperties} ${abiertaB2? styles.openCard : styles.cardClosed}`}>
+                    <div className={`${styles.infoCardProperties} ${abiertaB2? styles.openCard : styles.cardClosed} ${nunito.className}`}>
                         <InfoCard info = {"05-Nov  |  Comienzo de la jornada"} info2={"06-Nov  |  intervalo"} info3={"07-Nov  |  Cierre de la jornada"} sede={false} url={[]}/>
                     </div>
                 </div>
@@ -91,7 +92,7 @@ export default function Home(){
                                 <Image src={'/icons/deployIcon.png'} alt={"Icono para desplegar la card con información"} width={25} height={15} />
                             </button>
                         </div>
-                        <div className={`${styles.todo} ${cactus.className}`}>
+                        <div className={`${styles.todo} ${nunito.className}`}>
                             <div className={`${styles.imageCardProperties}`}>
                                 <Image src={'/icons/hatIcon.png'} alt={"Icono de gorro de graduación"} width={32} height={32}/>
                             </div>
@@ -101,7 +102,7 @@ export default function Home(){
                             </div>
                         </div>
                     </div>
-                    <div className={`${styles.infoCardProperties} ${abiertaB3? styles.openCard : styles.cardClosed}`}>
+                    <div className={`${styles.infoCardProperties} ${abiertaB3? styles.openCard : styles.cardClosed} ${nunito.className}`}>
                         <InfoCard info = {"Al momento las jornadas han sido declaradas de interés Municipal\n" +
                             "                        y han sido auspiciadas por la Sociedad Argentina de Pediatría"} sede={false} url={[]}/>
                     </div>
@@ -110,6 +111,9 @@ export default function Home(){
             <div className={`${styles.documentProperties} ${hovered ? styles.activa : ""}`}>
                 <CardAndSponsors />
             </div>
-        </div>
+            <div className={`${styles.secondarySponsors} ${cactus.className}`}>
+                hola aca van los futuros sponsors :(
+            </div>
+        </main>
     );
 }
