91102b9baf035707c969fddf0423d628ec56af2e
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-08-13 14:34:56 -0300

    mejoro sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: 'https://proyecto-jornadas-interhospitalaria.vercel.app/',
-  generateRobotsTxt: true,
+  siteUrl: 'https://proyecto-jornadas-interhospitalaria.vercel.app/', 
+  generateRobotsTxt: true,                     
+  sitemapSize: 7000,
+  changefreq: 'daily',                         
+  outDir: './public',                          
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*' , allow: '/' }
+    ]
+  },
 };
 
 export default config;
