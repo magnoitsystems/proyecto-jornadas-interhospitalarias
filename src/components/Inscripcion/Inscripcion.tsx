a21c8b92daf45bb18f1bec245ffbf91aa6f57ed4
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-08-18 22:32:48 -0300

    change data in the post of users

--- src/components/Inscripcion/Inscripcion.tsx
+++ src/components/Inscripcion/Inscripcion.tsx
@@ -135,7 +135,7 @@ export default function InscriptionForm() {
                 <option value="estudiante">Estudiante</option>
                 <option value="enfermero">Enfermero</option>
                 <option value="kinesiologo">Kinesiólogo</option>
-                <option value="fonoudiologo">Fonoaudiólogo</option>
+                <option value="fonoaudiologo">Fonoaudiólogo</option>
                 <option value="tecnico">Técnico</option>
                 <option value="no perteneciente al área de la salud">No perteneciente al área de la salud</option>
                 <option value="otros">Otros</option>
