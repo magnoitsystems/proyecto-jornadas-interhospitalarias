8573784e1d63d9bebf5aa8588004520fb74c4971
Agostina Bidegain <agostinabidegain@gmail.com>
2025-08-21 19:38:24 -0300

    fix a detail

--- src/components/InstructionsToPostWorks/instructions.tsx
+++ src/components/InstructionsToPostWorks/instructions.tsx
@@ -11,30 +11,35 @@ export default function InstructionsToPostWorks() {
                     </a>
                 </li>
                 <li className={cactus.className}>
-
                     <a href="#opcion-premio">
-                        Ver información acerca de
-                        “Opción de presentación a premio”
+                        Ver información acerca de “Opción de presentación a premio”
                     </a>
                 </li>
                 <li className={cactus.className}>
-
-                    <a href="#posters-digitales">
-                        Ver instrucciones para el armado de
-                        pósters digitales
+                    <a href="#anonimato">
+                        Ver información acerca de Anonimato de Pacientes – Consentimiento Informado
                     </a>
                 </li>
-                 <li className={cactus.className}>
-
-                     <a
-                         href="https://www.sap.org.ar/nosotros/institucional/direccion-de-publicaciones/para-autores"
-                         target="_blank"
-                         rel="noopener noreferrer"
-                     >
-                         Ver pautas del Reglamento de Publicaciones de Archivos Argentinos de Pediatría
-                     </a>
-                 </li>
+                <li className={cactus.className}>
+                    <a href="#tipos-trabajos">
+                        Ver tipos de Trabajos
+                    </a>
+                </li>
+                <li className={cactus.className}>
+                    <a href="#posters-digitales">
+                        Ver instrucciones para el armado de pósters digitales
+                    </a>
+                </li>
+                <li className={cactus.className}>
+                    <a
+                        href="https://www.sap.org.ar/nosotros/institucional/direccion-de-publicaciones/para-autores"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Ver pautas del Reglamento de Publicaciones de Archivos Argentinos de Pediatría
+                    </a>
+                </li>
             </ul>
         </nav>
-);
+    );
 }
