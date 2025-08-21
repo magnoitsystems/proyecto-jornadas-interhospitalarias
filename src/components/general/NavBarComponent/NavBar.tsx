70ca1c66768c786255cf738eff03151173f63830
Agostina Bidegain <agostinabidegain@gmail.com>
2025-08-21 18:38:24 -0300

    fix home background quality

--- src/components/general/NavBarComponent/NavBar.tsx
+++ src/components/general/NavBarComponent/NavBar.tsx
@@ -66,7 +66,7 @@ export default function NavBar(): JSX.Element {
 
     const bgUrl = (!isFormPage || isAdminPage)
         ? (isMobile && isHomeLike(pathname)
-            ? '/backgrounds/mob.svg'
+            ? '/backgrounds/homeMobile.svg'
             : getBackground(pathname))
         : 'none';
 
